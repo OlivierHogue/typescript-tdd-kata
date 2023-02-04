44e08961fbc4d239ed7547791cedf7c82bf45d1f
OlivierHogue <98484605+OlivierHogue@users.noreply.github.com>
2023-02-04 13:44:42 -0500

    fizzbuzz done

--- src/__tests__/fizzbuzz.test.ts
+++ src/__tests__/fizzbuzz.test.ts
@@ -1,33 +1,25 @@
-// import hello from "../hello";
 import fizzbuzz from "../fizzbuzz";
-describe('FizzBuzz tdd test', () => {
 
-    beforeEach( () => {
-	})
+describe('FizzBuzz tdd test', () => {
+    let expectedValuesNone: string[] = ["1"];
+    let expectedValuesFizz: string[] = ["1","2","Fizz"]
+    let expectedValuesBuzz: string[] = ["1","2","Fizz","4","Buzz"];
+    let expectedValuesFizzBuzz: string[] = 
+        ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"];
 
     it('Should return the number if its not divisible by 3 or 5', () => {
-        expect(fizzbuzz(1)).toEqual(1)
+        expect(fizzbuzz(1)).toEqual(expectedValuesNone);
+    });
+
+    it('should return Fizz if the number is divisible by 3', ()=> {
+        expect(fizzbuzz(3)).toEqual(expectedValuesFizz);
+    });
+
+    it('should return Buzz if the number is divisible by 5.', ()=>{
+        expect(fizzbuzz(5)).toEqual(expectedValuesBuzz);
+    });
+
+    it('shoule return FizzBuzz if the number is divisible by 3 and 5.', ()=>{
+        expect(fizzbuzz(15)).toEqual(expectedValuesFizzBuzz);
     });
 });
-
-
-// Given an integer n, return a string array answer (1-indexed) where:
-
-// answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
-// answer[i] == "Fizz" if i is divisible by 3.
-// answer[i] == "Buzz" if i is divisible by 5.
-// answer[i] == i if non of the above conditions are true.
- 
-
-// Example 1:
-
-// Input: n = 3
-// Output: ["1","2","Fizz"]
-// Example 2:
-
-// Input: n = 5
-// Output: ["1","2","Fizz","4","Buzz"]
-// Example 3:
-
-// Input: n = 15
-// Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,3 +1,21 @@
-export default function fizzbuzz(number: number){
-    return number;
+export default function fizzbuzz(n: number){
+    let rep: string[];
+    rep = [];
+    for(let i:number = 1; i <= n; i++){
+        
+        if(((i % 3) === 0 ) && ((i % 5) === 0)){
+            rep.push('FizzBuzz');
+        }
+        else if((i % 3) === 0){
+            rep.push('Fizz');    
+        }
+        else if((i % 5) === 0){
+            rep.push('Buzz');
+        }
+        else{
+            rep.push(i.toString());
+        }
+    }
+
+    return rep;
 }
